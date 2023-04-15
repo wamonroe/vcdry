033c6ba31dcd16e0b38eecb3c0856c9b5a2b24d5
Alex Monroe <alex@monroepost.com>
2023-04-15 14:18:41 -0400

    Fix Ruby 3.0 specs

--- spec/dummy/app/models/application_record.rb
+++ spec/dummy/app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
-  if Gem.loaded_specs["rails"].version >= "7.0"
+  if Gem.loaded_specs["rails"].version >= Gem::Version.new("7.0.0")
     primary_abstract_class
   else
     self.abstract_class = true
