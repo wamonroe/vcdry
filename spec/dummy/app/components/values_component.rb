c9dd6f32ce7711c23e07213cec977aa774e2c1c6
Alex Monroe <alex@monroepost.com>
2023-04-17 22:41:31 -0400

    Expanded tests for keywords with values

--- spec/dummy/app/components/values_component.rb
+++ spec/dummy/app/components/values_component.rb
@@ -1,4 +1,5 @@
 class ValuesComponent < ApplicationComponent
   keyword :size, :symbol, values: %i[sm md lg], optional: true
   keyword :padding, :symbol, default: :md, values: %i[sm md lg]
+  keyword :margin, :symbol, default: :md, values: %i[sm md lg], optional: true
 end
--- spec/lib/vcdry/values_spec.rb
+++ spec/lib/vcdry/values_spec.rb
@@ -5,13 +5,20 @@ RSpec.describe "VCDry::DSL" do
     context "with optional: true" do
       it "is expected to allow nil when optional: true" do
         expect { ValuesComponent.new(size: nil) }.not_to raise_error
-        expect(ValuesComponent.new).to have_instance_variable(:@size, with_value: nil)
+        expect(ValuesComponent.new(size: nil)).to have_instance_variable(:@size, with_value: nil)
       end
     end
 
     context "with :default" do
       it "is expected to not to allow an explicitly passed nil" do
         expect { ValuesComponent.new(padding: nil) }.to raise_error(VCDry::InvalidEnumValueError)
+      end
+    end
+
+    context "with optional: true and :default" do
+      it "is expected to allow nil" do
+        expect { ValuesComponent.new(margin: nil) }.not_to raise_error
+        expect(ValuesComponent.new(margin: nil)).to have_instance_variable(:@margin, with_value: nil)
       end
     end
   end
